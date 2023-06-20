986f0de24d76aa1048d3a4f9a020d84f24e711fa
Rohit Biswas <biswasrohit143@gmail.com>
2023-06-20 17:52:24 +0530

    Update deepstream_test_3.py

--- apps/adapt-v1/deepstream_test_3.py
+++ apps/adapt-v1/deepstream_test_3.py
@@ -18,11 +18,13 @@
 ################################################################################
 
 import sys
+
 sys.path.append('../')
 from pathlib import Path
 import gi
 import configparser
 import argparse
+
 gi.require_version('Gst', '1.0')
 from gi.repository import GLib, Gst
 from ctypes import *
@@ -41,26 +43,27 @@ silent = False
 file_loop = False
 perf_data = None
 
-MAX_DISPLAY_LEN=64
+MAX_DISPLAY_LEN = 64
 PGIE_CLASS_ID_VEHICLE = 0
 PGIE_CLASS_ID_BICYCLE = 1
 PGIE_CLASS_ID_PERSON = 2
 PGIE_CLASS_ID_ROADSIGN = 3
-MUXER_OUTPUT_WIDTH=1920
-MUXER_OUTPUT_HEIGHT=1080
-MUXER_BATCH_TIMEOUT_USEC=4000000
-TILED_OUTPUT_WIDTH=1280
-TILED_OUTPUT_HEIGHT=720
-GST_CAPS_FEATURES_NVMM="memory:NVMM"
-OSD_PROCESS_MODE= 0
-OSD_DISPLAY_TEXT= 1
-pgie_classes_str= ["Vehicle", "TwoWheeler", "Person","RoadSign"]
+MUXER_OUTPUT_WIDTH = 1920
+MUXER_OUTPUT_HEIGHT = 1080
+MUXER_BATCH_TIMEOUT_USEC = 4000000
+TILED_OUTPUT_WIDTH = 1280
+TILED_OUTPUT_HEIGHT = 720
+GST_CAPS_FEATURES_NVMM = "memory:NVMM"
+OSD_PROCESS_MODE = 0
+OSD_DISPLAY_TEXT = 1
+pgie_classes_str = ["Vehicle", "TwoWheeler", "Person", "RoadSign"]
+
 
 # pgie_src_pad_buffer_probe  will extract metadata received on tiler sink pad
 # and update params for drawing rectangle, object information etc.
-def pgie_src_pad_buffer_probe(pad,info,u_data):
-    frame_number=0
-    num_rects=0
+def pgie_src_pad_buffer_probe(pad, info, u_data):
+    frame_number = 0
+    num_rects = 0
     got_fps = False
     gst_buffer = info.get_buffer()
     if not gst_buffer:
@@ -82,28 +85,29 @@ def pgie_src_pad_buffer_probe(pad,info,u_data):
         except StopIteration:
             break
 
-        frame_number=frame_meta.frame_num
-        l_obj=frame_meta.obj_meta_list
+        frame_number = frame_meta.frame_num
+        l_obj = frame_meta.obj_meta_list
         num_rects = frame_meta.num_obj_meta
         obj_counter = {
-        PGIE_CLASS_ID_VEHICLE:0,
-        PGIE_CLASS_ID_PERSON:0,
-        PGIE_CLASS_ID_BICYCLE:0,
-        PGIE_CLASS_ID_ROADSIGN:0
+            PGIE_CLASS_ID_VEHICLE: 0,
+            PGIE_CLASS_ID_PERSON: 0,
+            PGIE_CLASS_ID_BICYCLE: 0,
+            PGIE_CLASS_ID_ROADSIGN: 0
         }
         while l_obj is not None:
-            try: 
+            try:
                 # Casting l_obj.data to pyds.NvDsObjectMeta
-                obj_meta=pyds.NvDsObjectMeta.cast(l_obj.data)
+                obj_meta = pyds.NvDsObjectMeta.cast(l_obj.data)
             except StopIteration:
                 break
             obj_counter[obj_meta.class_id] += 1
-            try: 
-                l_obj=l_obj.next
+            try:
+                l_obj = l_obj.next
             except StopIteration:
                 break
         if not silent:
-            print("Frame Number=", frame_number, "Number of Objects=",num_rects,"Vehicle_count=",obj_counter[PGIE_CLASS_ID_VEHICLE],"Person_count=",obj_counter[PGIE_CLASS_ID_PERSON])
+            print("Frame Number=", frame_number, "Number of Objects=", num_rects, "Vehicle_count=",
+                  obj_counter[PGIE_CLASS_ID_VEHICLE], "Person_count=", obj_counter[PGIE_CLASS_ID_PERSON])
 
         # Update frame rate through this probe
         stream_index = "stream{0}".format(frame_meta.pad_index)
@@ -111,44 +115,44 @@ def pgie_src_pad_buffer_probe(pad,info,u_data):
         perf_data.update_fps(stream_index)
 
         try:
-            l_frame=l_frame.next
+            l_frame = l_frame.next
         except StopIteration:
             break
 
     return Gst.PadProbeReturn.OK
 
 
-
-def cb_newpad(decodebin, decoder_src_pad,data):
+def cb_newpad(decodebin, decoder_src_pad, data):
     print("In cb_newpad\n")
-    caps=decoder_src_pad.get_current_caps()
+    caps = decoder_src_pad.get_current_caps()
     if not caps:
         caps = decoder_src_pad.query_caps()
-    gststruct=caps.get_structure(0)
-    gstname=gststruct.get_name()
-    source_bin=data
-    features=caps.get_features(0)
+    gststruct = caps.get_structure(0)
+    gstname = gststruct.get_name()
+    source_bin = data
+    features = caps.get_features(0)
 
     # Need to check if the pad created by the decodebin is for video and not
     # audio.
-    print("gstname=",gstname)
-    if(gstname.find("video")!=-1):
+    print("gstname=", gstname)
+    if (gstname.find("video") != -1):
         # Link the decodebin pad only if decodebin has picked nvidia
         # decoder plugin nvdec_*. We do this by checking if the pad caps contain
         # NVMM memory features.
-        print("features=",features)
+        print("features=", features)
         if features.contains("memory:NVMM"):
             # Get the source bin ghost pad
-            bin_ghost_pad=source_bin.get_static_pad("src")
+            bin_ghost_pad = source_bin.get_static_pad("src")
             if not bin_ghost_pad.set_target(decoder_src_pad):
                 sys.stderr.write("Failed to link decoder src pad to source bin ghost pad\n")
         else:
             sys.stderr.write(" Error: Decodebin did not pick nvidia decoder plugin.\n")
 
-def decodebin_child_added(child_proxy,Object,name,user_data):
+
+def decodebin_child_added(child_proxy, Object, name, user_data):
     print("Decodebin child added:", name, "\n")
-    if(name.find("decodebin") != -1):
-        Object.connect("child-added",decodebin_child_added,user_data)
+    if (name.find("decodebin") != -1):
+        Object.connect("child-added", decodebin_child_added, user_data)
 
     if "source" in name:
         source_element = child_proxy.get_by_name("source")
@@ -156,15 +160,14 @@ def decodebin_child_added(child_proxy,Object,name,user_data):
             Object.set_property("drop-on-latency", True)
 
 
-
-def create_source_bin(index,uri):
+def create_source_bin(index, uri):
     print("Creating source bin")
 
     # Create a source GstBin to abstract this bin's content from the rest of the
     # pipeline
-    bin_name="source-bin-%02d" %index
+    bin_name = "source-bin-%02d" % index
     print(bin_name)
-    nbin=Gst.Bin.new(bin_name)
+    nbin = Gst.Bin.new(bin_name)
     if not nbin:
         sys.stderr.write(" Unable to create source bin \n")
 
@@ -173,37 +176,38 @@ def create_source_bin(index,uri):
     # stream and the codec and plug the appropriate demux and decode plugins.
     if file_loop:
         # use nvurisrcbin to enable file-loop
-        uri_decode_bin=Gst.ElementFactory.make("nvurisrcbin", "uri-decode-bin")
+        uri_decode_bin = Gst.ElementFactory.make("nvurisrcbin", "uri-decode-bin")
         uri_decode_bin.set_property("file-loop", 1)
         uri_decode_bin.set_property("cudadec-memtype", 0)
     else:
-        uri_decode_bin=Gst.ElementFactory.make("uridecodebin", "uri-decode-bin")
+        uri_decode_bin = Gst.ElementFactory.make("uridecodebin", "uri-decode-bin")
     if not uri_decode_bin:
         sys.stderr.write(" Unable to create uri decode bin \n")
     # We set the input uri to the source element
-    uri_decode_bin.set_property("uri",uri)
+    uri_decode_bin.set_property("uri", uri)
     # Connect to the "pad-added" signal of the decodebin which generates a
     # callback once a new pad for raw data has beed created by the decodebin
-    uri_decode_bin.connect("pad-added",cb_newpad,nbin)
-    uri_decode_bin.connect("child-added",decodebin_child_added,nbin)
+    uri_decode_bin.connect("pad-added", cb_newpad, nbin)
+    uri_decode_bin.connect("child-added", decodebin_child_added, nbin)
 
     # We need to create a ghost pad for the source bin which will act as a proxy
     # for the video decoder src pad. The ghost pad will not have a target right
     # now. Once the decode bin creates the video decoder and generates the
     # cb_newpad callback, we will set the ghost pad target to the video decoder
     # src pad.
-    Gst.Bin.add(nbin,uri_decode_bin)
-    bin_pad=nbin.add_pad(Gst.GhostPad.new_no_target("src",Gst.PadDirection.SRC))
+    Gst.Bin.add(nbin, uri_decode_bin)
+    bin_pad = nbin.add_pad(Gst.GhostPad.new_no_target("src", Gst.PadDirection.SRC))
     if not bin_pad:
         sys.stderr.write(" Failed to add ghost pad in source bin \n")
         return None
     return nbin
 
+
 def main(args, requested_pgie=None, config=None, disable_probe=False):
     global perf_data
     perf_data = PERF_DATA(len(args))
 
-    number_sources=len(args)
+    number_sources = len(args)
 
     # Standard GStreamer initialization
     Gst.init(None)
@@ -225,27 +229,27 @@ def main(args, requested_pgie=None, config=None, disable_probe=False):
 
     pipeline.add(streammux)
     for i in range(number_sources):
-        print("Creating source_bin ",i," \n ")
-        uri_name=args[i]
-        if uri_name.find("rtsp://") == 0 :
+        print("Creating source_bin ", i, " \n ")
+        uri_name = args[i]
+        if uri_name.find("rtsp://") == 0:
             is_live = True
-        source_bin=create_source_bin(i, uri_name)
+        source_bin = create_source_bin(i, uri_name)
         if not source_bin:
             sys.stderr.write("Unable to create source bin \n")
         pipeline.add(source_bin)
-        padname="sink_%u" %i
-        sinkpad= streammux.get_request_pad(padname) 
+        padname = "sink_%u" % i
+        sinkpad = streammux.get_request_pad(padname)
         if not sinkpad:
             sys.stderr.write("Unable to create sink pad bin \n")
-        srcpad=source_bin.get_static_pad("src")
+        srcpad = source_bin.get_static_pad("src")
         if not srcpad:
             sys.stderr.write("Unable to create src pad bin \n")
         srcpad.link(sinkpad)
-    queue1=Gst.ElementFactory.make("queue","queue1")
-    queue2=Gst.ElementFactory.make("queue","queue2")
-    queue3=Gst.ElementFactory.make("queue","queue3")
-    queue4=Gst.ElementFactory.make("queue","queue4")
-    queue5=Gst.ElementFactory.make("queue","queue5")
+    queue1 = Gst.ElementFactory.make("queue", "queue1")
+    queue2 = Gst.ElementFactory.make("queue", "queue2")
+    queue3 = Gst.ElementFactory.make("queue", "queue3")
+    queue4 = Gst.ElementFactory.make("queue", "queue4")
+    queue5 = Gst.ElementFactory.make("queue", "queue5")
     pipeline.add(queue1)
     pipeline.add(queue2)
     pipeline.add(queue3)
@@ -255,7 +259,7 @@ def main(args, requested_pgie=None, config=None, disable_probe=False):
     nvdslogger = None
 
     print("Creating Pgie \n ")
-    if requested_pgie != None and (requested_pgie == 'nvinferserver' or requested_pgie == 'nvinferserver-grpc') :
+    if requested_pgie != None and (requested_pgie == 'nvinferserver' or requested_pgie == 'nvinferserver-grpc'):
         pgie = Gst.ElementFactory.make("nvinferserver", "primary-inference")
     elif requested_pgie != None and requested_pgie == 'nvinfer':
         pgie = Gst.ElementFactory.make("nvinfer", "primary-inference")
@@ -267,11 +271,11 @@ def main(args, requested_pgie=None, config=None, disable_probe=False):
 
     if disable_probe:
         # Use nvdslogger for perf measurement instead of probe function
-        print ("Creating nvdslogger \n")
+        print("Creating nvdslogger \n")
         nvdslogger = Gst.ElementFactory.make("nvdslogger", "nvdslogger")
 
     print("Creating tiler \n ")
-    tiler=Gst.ElementFactory.make("nvmultistreamtiler", "nvtiler")
+    tiler = Gst.ElementFactory.make("nvmultistreamtiler", "nvtiler")
     if not tiler:
         sys.stderr.write(" Unable to create tiler \n")
     print("Creating nvvidconv \n ")
@@ -282,8 +286,8 @@ def main(args, requested_pgie=None, config=None, disable_probe=False):
     nvosd = Gst.ElementFactory.make("nvdsosd", "onscreendisplay")
     if not nvosd:
         sys.stderr.write(" Unable to create nvosd \n")
-    nvosd.set_property('process-mode',OSD_PROCESS_MODE)
-    nvosd.set_property('display-text',OSD_DISPLAY_TEXT)
+    nvosd.set_property('process-mode', OSD_PROCESS_MODE)
+    nvosd.set_property('display-text', OSD_DISPLAY_TEXT)
 
     if file_loop:
         if is_aarch64():
@@ -329,17 +333,18 @@ def main(args, requested_pgie=None, config=None, disable_probe=False):
         pgie.set_property('config-file-path', config)
     else:
         pgie.set_property('config-file-path', "dstest3_pgie_config.txt")
-    pgie_batch_size=pgie.get_property("batch-size")
-    if(pgie_batch_size != number_sources):
-        print("WARNING: Overriding infer-config batch-size",pgie_batch_size," with number of sources ", number_sources," \n")
-        pgie.set_property("batch-size",number_sources)
-    tiler_rows=int(math.sqrt(number_sources))
-    tiler_columns=int(math.ceil((1.0*number_sources)/tiler_rows))
-    tiler.set_property("rows",tiler_rows)
-    tiler.set_property("columns",tiler_columns)
+    pgie_batch_size = pgie.get_property("batch-size")
+    if (pgie_batch_size != number_sources):
+        print("WARNING: Overriding infer-config batch-size", pgie_batch_size, " with number of sources ",
+              number_sources, " \n")
+        pgie.set_property("batch-size", number_sources)
+    tiler_rows = int(math.sqrt(number_sources))
+    tiler_columns = int(math.ceil((1.0 * number_sources) / tiler_rows))
+    tiler.set_property("rows", tiler_rows)
+    tiler.set_property("columns", tiler_columns)
     tiler.set_property("width", TILED_OUTPUT_WIDTH)
     tiler.set_property("height", TILED_OUTPUT_HEIGHT)
-    sink.set_property("qos",0)
+    sink.set_property("qos", 0)
 
     print("Adding elements to Pipeline \n")
     pipeline.add(pgie)
@@ -364,14 +369,14 @@ def main(args, requested_pgie=None, config=None, disable_probe=False):
     nvvidconv.link(queue4)
     queue4.link(nvosd)
     nvosd.link(queue5)
-    queue5.link(sink)   
+    queue5.link(sink)
 
     # create an event loop and feed gstreamer bus mesages to it
     loop = GLib.MainLoop()
     bus = pipeline.get_bus()
     bus.add_signal_watch()
-    bus.connect ("message", bus_call, loop)
-    pgie_src_pad=pgie.get_static_pad("src")
+    bus.connect("message", bus_call, loop)
+    pgie_src_pad = pgie.get_static_pad("src")
     if not pgie_src_pad:
         sys.stderr.write(" Unable to get src pad \n")
     else:
@@ -386,7 +391,7 @@ def main(args, requested_pgie=None, config=None, disable_probe=False):
         print(i, ": ", source)
 
     print("Starting pipeline \n")
-    # start play back and listed to events		
+    # start play back and listed to events
     pipeline.set_state(Gst.State.PLAYING)
     try:
         loop.run()
@@ -396,10 +401,10 @@ def main(args, requested_pgie=None, config=None, disable_probe=False):
     print("Exiting app\n")
     pipeline.set_state(Gst.State.NULL)
 
-def parse_args():
 
+def parse_args():
     parser = argparse.ArgumentParser(prog="deepstream_test_3",
-                    description="deepstream-test3 multi stream, multi model inference reference app")
+                                     description="deepstream-test3 multi stream, multi model inference reference app")
     parser.add_argument(
         "-i",
         "--input",
@@ -470,19 +475,19 @@ def parse_args():
     file_loop = args.file_loop
 
     if config and not pgie or pgie and not config:
-        sys.stderr.write ("\nEither pgie or configfile is missing. Please specify both! Exiting...\n\n\n\n")
+        sys.stderr.write("\nEither pgie or configfile is missing. Please specify both! Exiting...\n\n\n\n")
         parser.print_help()
         sys.exit(1)
     if config:
         config_path = Path(config)
         if not config_path.is_file():
-            sys.stderr.write ("Specified config-file: %s doesn't exist. Exiting...\n\n" % config)
+            sys.stderr.write("Specified config-file: %s doesn't exist. Exiting...\n\n" % config)
             sys.exit(1)
 
     print(vars(args))
     return stream_paths, pgie, config, disable_probe
 
+
 if __name__ == '__main__':
     stream_paths, pgie, config, disable_probe = parse_args()
     sys.exit(main(stream_paths, pgie, config, disable_probe))
-
